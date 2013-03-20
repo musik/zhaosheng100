b4a4203bc32bab228b3dca537a5437f676a58488
musik <860577@gmail.com>
2013-03-20 15:22:39 +0800

    lang

---
+++ lang/zh-cn/company.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	[Destoon B2B System] Copyright (c) 2008-2011 Destoon.COM
+	This is NOT a freeware, use is subject to license.txt
+*/
+defined('IN_DESTOON') or exit('Access Denied');
+$L['guest_title'] = '您访问的学校尚未注册本站会员';
+$L['not_company'] = '学校不存在';
+$L['news_title'] = '学校新闻';
+?>
---
+++ lang/zh-cn/homepage.inc.php
@@ -0,0 +1,15 @@
+<?php
+/*
+	[Destoon B2B System] Copyright (c) 2008-2011 Destoon.COM
+	This is NOT a freeware, use is subject to license.txt
+*/
+defined('IN_DESTOON') or exit('Access Denied');
+$L['msg_buy_deny'] = '您所在的会员组无权查看采购清单';
+$L['msg_contact_deny'] = '您所在的会员组无权查看联系方式';
+$HMENU = $DMENU = array('学校介绍', '专业设置','招生信息', '新闻中心', '荣誉资质', '人才招聘', '联系方式', '学校相册',   '视频', '友情链接', '商城', '诚信档案');
+$MFILE = array('introduce','zhuanye','zhaosheng',  'news', 'honor', 'job', 'contact', 'photo',   'video', 'link','mall','credit','homepage');
+$HSIDE = array('网站公告', '新闻中心',  '联系方式', '站内搜索', '荣誉资质', '友情链接');
+$SFILE = array('announce', 'news',  'contact', 'search', 'honor', 'link');
+$HMAIN = array( '学校介绍',  '招生信息', '专业设置', '学校相册', '学校视频');
+$IFILE = array( 'introduce',  'info', 'zhuanye', 'photo', 'video');
+?>
